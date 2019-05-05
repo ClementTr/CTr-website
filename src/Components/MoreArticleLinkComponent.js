4629aa642ffc4d431c6599dba0a4375cae8e0112
ClementTr <clement.tailleur@gmail.com>
2019-05-05 16:14:09 +0200

    start work on more articles component

--- src/Components/MoreArticleLinkComponent.js
+++ src/Components/MoreArticleLinkComponent.js
@@ -6,55 +6,53 @@ const divStyle = {
 
 function MoreArticleLinkComponent (){
  return (
-   <div className="row mb-5 mt-5">
+    <div className="container">
+
+     <h2 className="">More Blog Posts</h2>
 
      <div className="col-md-12">
-       <h2 className="mb-4">More Blog Posts</h2>
+       <div className="media">
+         <div className="media-left">
+           <img src='img/img1.png' className="media-object media-img"/>
+         </div>
+         <div className="media-body">
+           <h4 className="">Neural Net</h4>
+           <span className="category">Lifestyle</span>
+           <span className="mr-2">March 15, 2018 </span>
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
+       </div>
      </div>
 
      <div className="col-md-12">
-
-       <div className="post-entry-horzontal">
-         <a href="blog-single.html">
-           <div className="image element-animate" data-animate-effect="fadeIn" style={divStyle}></div>
-           <span className="text">
-             <div className="post-meta">
-               <span className="category">Travel</span>
-               <span className="mr-2">March 15, 2018 </span>
-             </div>
-             <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
-           </span>
-         </a>
+       <div className="media">
+         <div className="media-left">
+           <img src='img/img1.png' className="media-object media-img"/>
+         </div>
+         <div className="media-body">
+           <h4 className="">Architecture</h4>
+           <span className="category">Lifestyle</span>
+           <span className="mr-2">March 15, 2018 </span>
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
        </div>
-
-       <div className="post-entry-horzontal">
-         <a href="blog-single.html">
-           <div className="image element-animate" data-animate-effect="fadeIn" style={divStyle}></div>
-           <span className="text">
-             <div className="post-meta">
-               <span className="category">Lifestyle</span>
-               <span className="mr-2">March 15, 2018 </span>
-             </div>
-             <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
-           </span>
-         </a>
-       </div>
-
-       <div className="post-entry-horzontal">
-         <a href="blog-single.html">
-           <div className="image element-animate" data-animate-effect="fadeIn" style={divStyle}></div>
-           <span className="text">
-             <div className="post-meta">
-               <span className="category">Food</span>
-               <span className="mr-2">March 15, 2018 </span>
-             </div>
-             <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
-           </span>
-         </a>
-       </div>
-
      </div>
-   </div>
+
+     <div className="col-md-12">
+       <div className="media">
+         <div className="media-left">
+           <img src='img/img1.png' className="media-object media-img"/>
+         </div>
+         <div className="media-body">
+           <h4 className="">Natural Language Processing</h4>
+           <span className="category">Lifestyle</span>
+           <span className="mr-2">March 15, 2018 </span>
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
+       </div>
+     </div>
+
+    </div>
  )
 
 }
