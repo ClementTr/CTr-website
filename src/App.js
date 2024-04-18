08af78b8e857147f4c6a1d74523ccfa30a0ff83a
Clément Tailleur <clement.tailleur@gmail.com>
2024-04-18 12:33:43 +0200

    remove unused pictures and code (#26)
    Co-authored-by: Clément Tailleur <clement@macbook-pro-de-clement.home>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import PodcastComponent from './Components/Blog/Podcast/PodcastComponent'
 import NotFoundComponent from './Components/NotFoundComponent';
 import AboutComponent from './Components/About/AboutComponent';
 import HomeComponent from './Components/Home/HomeComponent';
@@ -20,7 +19,6 @@ class App extends Component {
 
           <Switch>
             <Route exact path='/' component={HomeComponent}></Route>
-            <Route exact path='/data-podcast' component={PodcastComponent}></Route>
             <Route exact path='/ctr-map' component={JourneyComponent}></Route>
             <Route exact path='/about' component={AboutComponent}></Route>
             <Route component={NotFoundComponent}></Route>
